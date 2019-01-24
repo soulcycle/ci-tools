09eae861c56358a46de4076616c2c867ee5f0544
Alex Mendoza <alex.mendoza@soul-cycle.com>
2019-01-23 21:40:51 -0500

    Adding bintray login and utils.sh

---
+++ bintray/bintray-login.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Attempts to log in to bintray using an api key
+#
+# NOTE: Assumes the following env vars are defined within the running environment
+#   - BINTRAY_USER - bintray user
+#   - BINTRAY_KEY - bintray api key
+#   - BINTRAY_REPO - A bintray repo name
+
+set -e
+
+# Set TOOL_ROOT, the location of the directory this script is housed in
+readonly TOOL_ROOT=$(cd $( dirname "${BASH_SOURCE[0]}" ) && pwd )
+
+# Include shell helpers
+source $TOOL_ROOT/utils.sh
+
+echo_yellow "Logging in to bintray with an api key..."
+
+# Login with Docker
+echo "$BINTRAY_KEY" | docker login -u $BINTRAY_USER --password-stdin $BINTRAY_REPO
+
+echo_green "Successfully logged in to bintray with an api key."
